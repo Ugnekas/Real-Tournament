6590bb8ce42f6cd8a02b442e35b87c993e54c930
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 12:16:18 +0200

    Weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public GameObject explosionParticles;
+	public GameObject bulletPrefab;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(bulletPrefab, transform.position, transform.rotation);
-            Instantiate(explosionParticles);
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Instantiate(bulletPrefab,transform.position,transform.rotation);
+		}
+	}
 }
